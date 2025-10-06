65846cae807a1631320558100e73cb43623be909
EliXorZz <dylan.battig@gmail.com>
2025-10-06 13:24:30 +0200

    fix migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('email');
             $table->string('phone');
-            $table->string('profile');
             $table->timestamps();
         });
     }
